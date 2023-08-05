770ccbab2993f0ec728985ce82bddf02bb92cbe6
David Welander <6464294+navday@users.noreply.github.com>
2023-08-05 13:55:06 +0200

    changed root

--- start-sling.sh
+++ start-sling.sh
@@ -2,8 +2,9 @@
 # Remember to make this scrip executable by running command chmod +x start-sling.sh
 # Function to start Apache Sling in the background
 start_sling() {
+    cd /home/nds1/sling-org-apache-sling-starter
     # Start Apache Sling Launcher as a background process and redirect output to /dev/null
-    nohup java -jar /home/nds1/sling-org-apache-sling-starter/target/dependency/org.apache.sling.feature.launcher.jar -f /home/nds1/sling-org-apache-sling-starter/target/slingfeature-tmp/feature-app.json > /dev/null 2>&1 &
+    nohup java -jar target/dependency/org.apache.sling.feature.launcher/bin/launcher -f target/slingfeature-tmp/feature-oak_tar.json > /dev/null 2>&1 &
 }
 
 # Call the start_sling function
