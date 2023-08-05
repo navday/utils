06229fb93dfcf02f5bddbd00e655202572135eb5
David Welander <6464294+navday@users.noreply.github.com>
2023-08-05 13:38:00 +0200

    changed root

--- start-sling.sh
+++ start-sling.sh
@@ -3,7 +3,7 @@
 # Function to start Apache Sling in the background
 start_sling() {
     # Start Apache Sling Launcher as a background process and redirect output to /dev/null
-    nohup java -jar ../sling-org-apache-sling-starter/target/dependency/org.apache.sling.feature.launcher.jar -f ../sling-org-apache-sling-starter/target/slingfeature-tmp/feature-app.json > /dev/null 2>&1 &
+    nohup java -jar /home/nds1/sling-org-apache-sling-starter/target/dependency/org.apache.sling.feature.launcher.jar -f /home/nds1/sling-org-apache-sling-starter/target/slingfeature-tmp/feature-app.json > /dev/null 2>&1 &
 }
 
 # Call the start_sling function
